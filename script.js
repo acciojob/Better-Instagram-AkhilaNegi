f48ff1041e4c3178b3f6c3b6950a9c384d2ecc7e
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-07-21 12:27:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-const parent = document.getElementById("parent");
-const images = document.querySelectorAll(".image");
+const parent = document.querySelector("#parent");
+let start = null;
 
-let selected = null;
-
-// Event listener for the drag start event
 parent.addEventListener("dragstart", (event) => {
-  selected = event.target;
-  event.dataTransfer.effectAllowed = "move";
-  event.dataTransfer.setData("text/html", selected.outerHTML);
+  start = event.target;
 });
 
-// Event listener for the drag over event
 parent.addEventListener("dragover", (event) => {
   event.preventDefault();
-  if (event.target.classList.contains("image")) {
-    event.target.classList.add("selected");
-  }
 });
 
-// Event listener for the drag leave event
-parent.addEventListener("dragleave", (event) => {
-  event.preventDefault();
-  if (event.target.classList.contains("image")) {
-    event.target.classList.remove("selected");
-  }
-});
-
-// Event listener for the drop event
 parent.addEventListener("drop", (event) => {
   event.preventDefault();
-  if (event.target.classList.contains("image")) {
-    event.target.insertAdjacentHTML("beforebegin", selected.outerHTML);
-    selected.remove();
-    event.target.classList.remove("selected");
+  const final = event.target;
+  const parent = final.parentNode;
+  if (final !== start) {
+    parent.insertBefore(start, final);
   }
 });
