0e426c0e9ebc57222f28c4f72e6bc8524d2ac34e
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-07-21 12:19:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,63 +1,28 @@
-//your code here
-// Get all the draggable elements
-const draggableElements = document.querySelectorAll('.image');
+let imageCollection = document.getElementsByClassName("image");
 
-// Variable to store the currently dragged element
-let dragSrcElement = null;
+let divToSwap = [];
 
-// Function to handle drag start event
-function handleDragStart(e) {
-  dragSrcElement = this;
-  e.dataTransfer.effectAllowed = 'move';
-  e.dataTransfer.setData('text/html', this.innerHTML);
-  this.classList.add('selected');
+for (let i = 0; i < imageCollection.length; i++) {
+    imageCollection[i].addEventListener("dragstart", function(event) {
+        divToSwap[0] = event.target.id;
+    });
+
+    imageCollection[i].addEventListener("dragover", function(event) {
+        event.preventDefault();
+    });
+
+    imageCollection[i].addEventListener("drop", function(event) {
+        divToSwap[1] = event.target.id;
+
+        if (
+            divToSwap[0] != divToSwap[1] &&
+            divToSwap[0] != "" &&
+            divToSwap[1] != ""
+        ) {
+            let div1 = document.getElementById(divToSwap[0]);
+            let div2 = document.getElementById(divToSwap[1]);
+            div1.setAttribute("id", divToSwap[1]);
+            div2.setAttribute("id", divToSwap[0]);
+        }
+    });
 }
-
-// Function to handle drag over event
-function handleDragOver(e) {
-  if (e.preventDefault) {
-    e.preventDefault();
-  }
-  e.dataTransfer.dropEffect = 'move';
-  return false;
-}
-
-// Function to handle drag enter event
-function handleDragEnter(e) {
-  this.classList.add('over');
-}
-
-// Function to handle drag leave event
-function handleDragLeave(e) {
-  this.classList.remove('over');
-}
-
-// Function to handle drop event
-function handleDrop(e) {
-  if (e.stopPropagation) {
-    e.stopPropagation();
-  }
-  if (dragSrcElement !== this) {
-    dragSrcElement.innerHTML = this.innerHTML;
-    this.innerHTML = e.dataTransfer.getData('text/html');
-  }
-  return false;
-}
-
-// Function to handle drag end event
-function handleDragEnd(e) {
-  this.classList.remove('selected');
-  draggableElements.forEach((element) => {
-    element.classList.remove('over');
-  });
-}
-
-// Add event listeners to the draggable elements
-draggableElements.forEach((element) => {
-  element.addEventListener('dragstart', handleDragStart, false);
-  element.addEventListener('dragenter', handleDragEnter, false);
-  element.addEventListener('dragover', handleDragOver, false);
-  element.addEventListener('dragleave', handleDragLeave, false);
-  element.addEventListener('drop', handleDrop, false);
-  element.addEventListener('dragend', handleDragEnd, false);
-});
