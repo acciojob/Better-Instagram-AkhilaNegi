44bf8413438910e65b04a41ceabdf8a6aac2591b
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-08-05 20:28:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-//your code here
-const draggableElements = document.querySelectorAll('.draggable');
-const container = document.getElementById('container');
-let dragStartIndex;
+const images = document.querySelectorAll('.image');
 
-draggableElements.forEach((element, index) => {
-  element.addEventListener('dragstart', () => {
-    dragStartIndex = index;
-  });
+let dragSrcEl = null;
 
-  element.addEventListener('dragover', (event) => {
-    event.preventDefault();
-  });
-
-  element.addEventListener('drop', () => {
-    const dragEndIndex = index;
-    swapElements(dragStartIndex, dragEndIndex);
-  });
-});
-
-function swapElements(fromIndex, toIndex) {
-  const fromElement = container.children[fromIndex];
-  const toElement = container.children[toIndex];
-  container.insertBefore(toElement, fromElement);
+function handleDragStart(e) {
+  this.style.opacity = '0.4';
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
 }
+
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
+  if (dragSrcEl !== this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+  return false;
+}
+
+function handleDragEnd(e) {
+  this.style.opacity = '1';
+  images.forEach(img => img.classList.remove('over'));
+}
+
+images.forEach(img => {
+  img.addEventListener('dragstart', handleDragStart, false);
+  img.addEventListener('dragover', handleDragOver, false);
+  img.addEventListener('drop', handleDrop, false);
+  img.addEventListener('dragend', handleDragEnd, false);
+});
